939f42d5ef36bfc7c9990325afe6afa2cacb2dd8
Kevin <sxzz@sxzz.moe>
2021-07-07 21:27:43 +0800

    feat: add vue2

---
+++ packages/vue2/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
+  extends: ['plugin:vue/vue-recommended', '@sxzz/eslint-config-ts'],
+  rules: {
+    'vue/max-attributes-per-line': ['warn', { singleline: 5 }],
+    'vue/no-v-html': 'off',
+  },
+};
