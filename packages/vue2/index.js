d6746ba4df6f69f0d7c9e64e762c00493483e1fb
Kevin <sxzz@sxzz.moe>
2021-07-21 09:34:45 +0800

    feat: support vue2 jsx

--- packages/vue2/index.js
+++ packages/vue2/index.js
@@ -5,6 +5,10 @@ module.exports = {
       parser: 'vue-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue'],
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
   ],
