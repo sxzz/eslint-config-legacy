79bdf8ed6c662ee4ca87c8638ad7a241fb68ed06
Kevin <sxzz@sxzz.moe>
2021-10-05 18:28:33 +0800

    feat: add rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -109,9 +109,6 @@ module.exports = defineConfig({
     // best-practice
     'array-callback-return': 'error',
     'block-scoped-var': 'error',
-    'consistent-return': 'off',
-    complexity: ['off', 11],
-    // eqeqeq: ['error', 'allow-null'],
     'no-alert': 'warn',
     'no-case-declarations': 'error',
     'no-fallthrough': ['warn', { commentPattern: 'break[\\s\\w]*omitted' }],
@@ -119,30 +116,56 @@ module.exports = defineConfig({
     'no-with': 'error',
     'no-void': 'error',
 
+    // stylistic-issues
+    'no-lonely-if': 'error',
+    'prefer-exponentiation-operator': 'error',
+
     // unicorns
-    // Pass error message when throwing errors
+    'unicorn/better-regex': 'error',
+    'unicorn/custom-error-definition': 'error',
     'unicorn/error-message': 'error',
-    // Uppercase regex escapes
     'unicorn/escape-case': 'error',
-    // Array.isArray instead of instanceof
+    'unicorn/explicit-length-check': 'error',
+    'unicorn/import-index': 'error',
+    'unicorn/new-for-builtins': 'error',
+    'unicorn/no-array-callback-reference': 'error',
+    'unicorn/no-array-method-this-argument': 'error',
+    'unicorn/no-array-push-push': 'error',
+    'unicorn/no-console-spaces': 'error',
+    'unicorn/no-for-loop': 'error',
+    'unicorn/no-hex-escape': 'error',
     'unicorn/no-instanceof-array': 'error',
-    // Prevent deprecated `new Buffer()`
+    'unicorn/no-invalid-remove-event-listener': 'error',
+    'unicorn/no-lonely-if': 'error',
+    'unicorn/no-new-array': 'error',
     'unicorn/no-new-buffer': 'error',
-    // Keep regex literals safe!
     'unicorn/no-unsafe-regex': 'off',
-    // Lowercase number formatting for octal, hex, binary (0x1'error' instead of 0X1'error')
     'unicorn/number-literal-case': 'error',
-    // ** instead of Math.pow()
-    'unicorn/prefer-exponentiation-operator': 'error',
-    // includes over indexOf when checking for existence
-    'unicorn/prefer-includes': 'error',
-    // String methods startsWith/endsWith instead of more complicated stuff
-    'unicorn/prefer-string-starts-ends-with': 'error',
-    // textContent instead of innerText
+    'unicorn/prefer-add-event-listener': 'error',
+    'unicorn/prefer-array-find': 'error',
+    'unicorn/prefer-array-flat-map': 'error',
+    'unicorn/prefer-array-index-of': 'error',
+    'unicorn/prefer-array-some': 'error',
+    'unicorn/prefer-date-now': 'error',
+    'unicorn/prefer-dom-node-append': 'error',
+    'unicorn/prefer-dom-node-dataset': 'error',
+    'unicorn/prefer-dom-node-remove': 'error',
     'unicorn/prefer-dom-node-text-content': 'error',
-    // Enforce throwing type error when throwing error while checking typeof
+    'unicorn/prefer-includes': 'error',
+    'unicorn/prefer-keyboard-event-key': 'error',
+    'unicorn/prefer-math-trunc': 'error',
+    'unicorn/prefer-modern-dom-apis': 'error',
+    'unicorn/prefer-negative-index': 'error',
+    'unicorn/prefer-number-properties': 'error',
+    'unicorn/prefer-optional-catch-binding': 'error',
+    'unicorn/prefer-prototype-methods': 'error',
+    'unicorn/prefer-query-selector': 'error',
+    'unicorn/prefer-reflect-apply': 'error',
+    'unicorn/prefer-string-replace-all': 'error',
+    'unicorn/prefer-string-slice': 'error',
+    'unicorn/prefer-string-starts-ends-with': 'error',
+    'unicorn/prefer-string-trim-start-end': 'error',
     'unicorn/prefer-type-error': 'error',
-    // Use new when throwing error
     'unicorn/throw-new-error': 'error',
   },
 });
